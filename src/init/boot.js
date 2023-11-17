11e54ab3c049fe31416a1f3bf64898c4e6ea619b
ingrid.mollo <ingrid.mollo@agetic.gob.bo>
2023-11-17 12:13:18 -0400

    Corregir el archivo de boot.js

--- src/init/boot.js
+++ src/init/boot.js
@@ -1,10 +1,18 @@
 const logger = require('../helpers/logger');
 
 function iniciarApp(app, env) {
-  const port = app.get('port') || 1800;
-  app.listen(port, () => {
-    logger.log('info', `[${env}]Servidor escuchando en el puerto  ${port}...`);
+ 
+  const port = app.configurations.config.services.puerto; 
+  const host = '127.0.0.1'; 
+  const hostIPv6 = '::1'; 
+
+  app.listen(port, host, () => {
+    logger.log('info', `[${env}] Servidor escuchando en ${host}:${port}...`);
   });
+  app.listen(port, hostIPv6, () => {
+    logger.log('info', `[${env}] Servidor escuchando en ${hostIPv6}:${port}...`);
+  });
+  
 }
 
 module.exports = iniciarApp;
