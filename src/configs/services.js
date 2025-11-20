705fbdab72e79925a61a8572f47fcbd2bec32d6c
vladimir.copa <vladimir.copa@agetic.gob.bo>
2025-11-20 09:11:38 -0400

    fix: no data

--- src/configs/services.js
+++ src/configs/services.js
@@ -1,6 +1,6 @@
 module.exports = {
-  usuario: 'davis.mendoza',
-  clave: '9515D00480BDB0D53E0E16D82C1F7E0F',
+  usuario: 'credencial',
+  clave: 'credencial',
   wsdl: 'http://srv.oopp.gob.bo/public/wsanboperadores.asmx?WSDL',
   wsdlVehiculos: 'http://srv.oopp.gob.bo/public/wsanbvehiculos.asmx?WSDL',
   wsdlUso:'http://srv.oopp.gob.bo/public/wsAgetic.asmx?WSDL',
